988765683912958af2568b90f43d9246efb443fc
maddes-b <maddes-b@users.noreply.github.com>
2024-03-10 12:32:10 +0100

    Update Workaround Script for Poorly Conceived IPv6 Support on DSM 7
    Use flock to avoid running in parallel

--- fix-ipv6-addresses-dsm7.sh
+++ fix-ipv6-addresses-dsm7.sh
@@ -1,32 +1,31 @@
 #!/bin/sh -eu
-### Workaround script as DSM 7 does not support IPv6 multi-homing as by design (2024-03: 7.2.1-Update 4)
-### https://community.synology.com/enu/forum/1/post/186660
+### Workaround script as DSM 7 does not support IPv6 design of being multi-homed and allowing static addresses and automatic confguration (SLAAC, DHCPv6 client) in parallel.
+### (2024-03: 7.2.1-Update 4)
+### see https://community.synology.com/enu/forum/1/post/186660
+### recommended path: /usr/local/sbin
+### https://kb.synology.com/en-us/DSM/tutorial/common_mistake_in_task_scheduler_script
+### Exit codes:
+### * 0 = all good, no errors
+### * 50 = started in parallel, did not execute
+### * see glibc: https://sourceware.org/git/?p=glibc.git;a=blob;f=misc/sysexits.h;hb=HEAD
+### * see https://tldp.org/LDP/abs/html/exitcodes.html
 
-### Avoid running in parallel (e.g. running each minute)
-PID_FILE='/run/fix-ipv6-addresses-dsm7.pid'
-if [ -s "${PID_FILE}" ]; then
-  PID="$(head -qn 1 "${PID_FILE}")"
-  if [ -d "/proc/${PID}" ]; then
-    RUNNING=0
-    CMDLINE="$(xargs -0 -I % printf -- "%\n" </proc/"${PID}"/cmdline)"
-    printf -- "${CMDLINE}" | while read ARGUMENT
-     do
-      if [ "${ARGUMENT}" == "${0}" ]; then
-        RUNNING=1
-        break
-      fi
-    done
-    if [ "${RUNNING}" -ne 0 ]; then
-      return 0 2>/dev/null || exit 0
-    fi
-  fi ## /proc/PID
-fi ## PID_FILE
-#
-printf -- '%s' "${$}" >"${PID_FILE}"
+SCRIPTNAME="$(basename "${0}")"
 
-### Variables and function to log that fixing took place
+## Avoid running in parallel (e.g. running each minute)
+if [ "${FLOCKER:-}" != "${0}" ]; then
+  RC=0
+  { /usr/bin/env FLOCKER="${0}" flock -en -E 50 "${0}" "${0}" "${@}" ; } || RC="${?}"
+  if [ "${RC}" -eq 50 ]; then
+    printf -- '%s\n' \
+      "ERROR/${SCRIPTNAME}: Running in parallel. Exiting."
+  fi
+  return "${RC}" 2>/dev/null || exit "${RC}"
+fi
+
+## Variables and function to log that fixing took place
 LOG_DIR='/volume1/<shared folder>/path/to/log' ## TODO: adapt to system
-LOG_FILE="${LOG_DIR}/ipv6_fixed.log"
+LOG_FILE="${LOG_DIR}/fix-ipv6-addresses-dsm7.log"
 if [ -d "${LOG_DIR}" ]; then
   LOG_LINE="$(date -Iseconds)${1:+ ${1}}"
   LOG_DONE=0
@@ -41,7 +40,7 @@ log_fixed () {
   fi
 }
 
-### Variables definitions
+## Variables definitions
 INTERFACE='ovs_bond0' ## TODO: adapt to system
 NEED_DHCPC=0
 NEED_RA=0
@@ -49,7 +48,7 @@ NEED_RA=0
 PREFIX_ULA1='fdxxxxxxxxxxxxxx' ## TODO: adapt to network
 PREFIX_GUA='2003'
 
-### Check sysctl settings: addr_gen_mode (not on DSM7 kernel), use_tempaddr, accept_ra_defrtr, accept_ra_pinfo, accept_ra
+## Check sysctl settings: addr_gen_mode (not on DSM7 kernel), use_tempaddr, accept_ra_defrtr, accept_ra_pinfo, accept_ra
 ## https://www.kernel.org/doc/Documentation/networking/ip-sysctl.txt
 #SYSCTL="$(sysctl -n "net.ipv6.conf.${INTERFACE}.addr_gen_mode")"
 #if [ "${SYSCTL}" -ne 0 ]; then
@@ -97,7 +96,7 @@ if [ "${SYSCTL}" -ne 2 ]; then
   NEED_RA=1
 fi
 
-### Check for ULA #1 (DHPCv6)
+## Check for ULA #1 (DHPCv6)
 RC=0
 grep -q -e "^${PREFIX_ULA1}.*${INTERFACE}\$" /proc/net/if_inet6 || RC="${?}"
 if [ "${RC}" -ne 0 ]; then
@@ -106,7 +105,7 @@ if [ "${RC}" -ne 0 ]; then
   NEED_DHCPC=1
 fi
 
-### Check for GUA (SLAAC)
+## Check for GUA (SLAAC)
 RC=0
 grep -q -e "^${PREFIX_GUA}.*${INTERFACE}\$" /proc/net/if_inet6 || RC="${?}"
 if [ "${RC}" -ne 0 ]; then
@@ -115,7 +114,7 @@ if [ "${RC}" -ne 0 ]; then
   NEED_RA=1
 fi
 
-### Check for default route
+## Check for default route
 ROUTE="$(ip -6 route show match default)"
 if [ -z "${ROUTE}" ]; then
   log_fixed
@@ -123,9 +122,9 @@ if [ -z "${ROUTE}" ]; then
   NEED_RA=1
 fi
 
-### Try to get IPv6 addresses
+## Try to get IPv6 addresses
 if [ "${NEED_DHCPC}" -ne 0 -o "${NEED_RA}" -ne 0 ]; then
-  /sbin/ip -6 addr show dev "${INTERFACE}"
+  /usr/sbin/ip -6 addr show dev "${INTERFACE}"
   #
   if [ "${NEED_DHCPC}" -ne 0 ]; then
     /usr/syno/bin/synosystemctl enable "dhclient6@${INTERFACE}-client.service"
@@ -133,13 +132,11 @@ if [ "${NEED_DHCPC}" -ne 0 -o "${NEED_RA}" -ne 0 ]; then
   fi
   #
   if [ "${NEED_RA}" -ne 0 ]; then
-    /sbin/rdisc6 -mnq "${INTERFACE}"
+    /usr/sbin/rdisc6 -mnq "${INTERFACE}"
   fi
   #
-  /sbin/ip -6 addr show dev "${INTERFACE}"
+  /usr/sbin/ip -6 addr show dev "${INTERFACE}"
   ip -6 route show match default
 fi
-
-rm "${PID_FILE}"
 
 return 0 2>/dev/null || exit 0
